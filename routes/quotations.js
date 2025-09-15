173b0bcba628abf8e1c931e5f4cff6223d2d1296
WolbachAuth0 <aaron.wolbach@auth0.com>
2025-09-15 13:52:19 -0400

    add security to api

--- routes/quotations.js
+++ routes/quotations.js
@@ -1,39 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/quotations');
-const { verifyJWT } = require('../middleware/auth');
+const { verifyJWT, checkJWTPermissions } = require('../middleware/auth');
 const validate = require('./../middleware/schemaValidator');
 
 module.exports = router
 
 router
   .route('/')
-  // .all(verifyJWT)
+  .all(verifyJWT)
   .get(
+    checkJWTPermissions(['read:quotation']),
     controller.search
   )
   .post(
+    checkJWTPermissions(['create:quotation']),
     validate.requestBody(controller.schemas.quotation),
     controller.create
   )
 
 router
   .route('/random')
-  // .all(verifyJWT)
+  .all(verifyJWT)
   .get(
     controller.getRandom
   )
 
 router
   .route('/:quotation_id')
-  // .all(verifyJWT)
+  .all(verifyJWT)
   .get(
+    checkJWTPermissions(['read:quotation']),
     controller.getById
   )
   .patch(
+    checkJWTPermissions(['update:quotation']),
     validate.requestBody(controller.schemas.quotation),
     controller.update
   )
   .delete(
+    checkJWTPermissions(['delete:quotation']),
     controller.remove
   )
